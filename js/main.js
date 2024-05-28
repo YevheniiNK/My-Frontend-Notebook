faa46b634e4b52af01dbac7b99bb0d88d6b3deb4
YevheniiNK <yevhenii.work.ua@gmail.com>
2024-05-29 02:52:39 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 
+// Бургер
 document.querySelector('.header__burger').onclick = function() {
+
+    // добавляем\убираем клас .active
    document.querySelector('.header__burger').classList.toggle('active');
-   document.querySelector('.header__menu').classList.toggle('active');     
+   document.querySelector('.header__menu').classList.toggle('active');
+
+    // запрещаем прокрутку странице при активном бургере 
+   document.querySelector('body').classList.toggle('lock');     
  }
 
 
