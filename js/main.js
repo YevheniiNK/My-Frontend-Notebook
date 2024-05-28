a6352da108ec5cb29cd6e7288a4b7d393f249f63
YevheniiNK <yevhenii.work.ua@gmail.com>
2024-05-29 02:27:29 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+document.querySelector('.header__burger').onclick = function() {
+   document.querySelector('.header__burger').classList.toggle('active');
+   document.querySelector('.header__menu').classList.toggle('active');     
+ }
+
+
+ 
+ 
